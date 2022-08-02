49b105b8aa06f1ac1d8e23eeef3c2158907c5ced
Joel Pires <joelpires70@gmail.com>
2022-08-02 16:45:16 -0300

    LCD with some bugs

--- keyboard.c
+++ keyboard.c
@@ -1,10 +1,13 @@
-int8 keyboard(){
-    /*  
+int8 keyboard(int L0, int C0){
+    /*
+    L0: Start address of pins to map the lines of keyboard. The next 3 address must be free.
+    C0: Start address of pins to map the columns of keyboard. The next 2 must be free. 
     Keyboard connected on PORTD of PIC18F4450.
     This function hundles the keyboard interaction.
     */
 
     //searching for the button pressioned
+    int8 i, j;
 
     int8 value[4][3] = {
         {1,2,3},
@@ -13,40 +16,28 @@ int8 keyboard(){
         {11,0,12} //11 = *, 12 = #
     };
 
-    /*
-    PIN_D0  31768
-    PIN_D1  31769
-    PIN_D2  31770
-    PIN_D3  31771
-    PIN_D4  31772
-    PIN_D5  31773
-    PIN_D6  31774
-    PIN_D7  31775
-    */
     int8 v = -1;
-    int8 i, j;
-
+    
     while (1) { //Keep running this while none of the key of keyboard is not pressioned
 
         for(i = 0; i < 4; i = i+1){ //for each line of the keyboard matrix
             
-            //set line i. D0 = 31768
-            //output_bit(48+i,1);
+            //set line i.
             for(j = 0; j < 3; j = j+1){ //for each column of the keyboard matrix, at line i
                 
-                output_bit(PIN_D4+j,1); //turn the column PIN_D4 + j to high state.
+                output_bit(C0+j,1); //turn the column C0 + j to high state.
                 
-                if (input(PIN_D0+i)){ //return only if the cell at line i and column j is high 
-                    output_bit(PIN_D4+j,0);
+                if (input(L0+i)){ //return only if the cell at line i and column j is high 
+                    output_bit(C0+j,0);
                     
-                    while (input(PIN_D0) | input(PIN_D1) | input(PIN_D2) | input(PIN_D3)); //wait user stop pressing the key to avoid repeated keys captured
+                    while (input(L0) | input(L0+1) | input(L0+2) | input(L0+3)); //wait user stop pressing the key to avoid repeated keys captured
                     return value[i][j]; //get the value
                 }
-                output_bit(PIN_D4+j,0);
+                output_bit(C0+j,0);
                 
             }
             
-            output_bit(PIN_D0+i,0);
+            output_bit(L0+i,0);
         }
     }
 
--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <18F4550.h>
 
+
 #device adc=8
 
 #fuses NOMCLR INTRC_IO
 
 #use delay(clock=16000000)
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
 //#use fast_io(B)
 #use i2c(master,Fast,sda=PIN_B0,scl=PIN_B1)
 //#use I2C(master, I2C1, FAST = 100000)
@@ -13,25 +15,31 @@
 #include "RTC_handler.c"
 #include "utils.c"
 
+#define LCD_TYPE 2
+#include <lcd.c>
+
+#include <stdio.h>
+#include <string.h>
+
 int8 two_d_keyboard(){
    /*
    Assuming the key pressioned is a number of only one digits
    Return a number formed by two keys pressioned
    */
    int8 two_digits = 0;
-   two_digits = 10*keyboard(); //H0
+   two_digits = 10*keyboard(PIN_A0, PIN_A4); //H0
 
-   output_bit(PIN_D7, 1); //Signal to tell if the two digits was captured or not
+   output_bit(PIN_E1, 1); //Signal to tell if the two digits was captured or not
    delay_ms(100);
-   output_bit(PIN_D7, 0);
+   output_bit(PIN_E1, 0);
 
-   two_digits = two_digits+keyboard();//H0+L = HL
+   two_digits = two_digits+keyboard(PIN_A0, PIN_A4);//H0+L = HL
 
-   output_bit(PIN_D7, 1); //Signal to tell if the two digits was captured or not
+   output_bit(PIN_E1, 1); //Signal to tell if the two digits was captured or not
    delay_ms(100);
-   output_bit(PIN_D7, 0);
+   output_bit(PIN_E1, 0);
 
-   return two_d_keyboard
+   return two_digits;
 }
 
 void set_datetime(){
@@ -39,7 +47,6 @@ void set_datetime(){
    The datetime is provided by the used from keyboard.
    */
 
-   //Set date time from keyboard
    ds1307_write(0, Dec2BCD(two_d_keyboard())); //seconds
    ds1307_write(1, Dec2BCD(two_d_keyboard())); //minute
    ds1307_write(2, Dec2BCD(two_d_keyboard())); //hour
@@ -52,8 +59,12 @@ void set_datetime(){
 
 void main()
 {
-   int second, minute, hour, day, date, month, year;
+   int seconds, minutes, hours, days, date, months, year;
+   char time[30];
+   char display[40] = "\f";
 
+   seconds = 10;
+   minutes = 10;
    setup_oscillator(OSC_16MHZ);                      // Set internal oscillator to 8MHz
    setup_adc_ports(NO_ANALOGS);
    port_b_pullups(TRUE);// Enable PORTB pull-ups:
@@ -61,21 +72,35 @@ void main()
     port_b_pullups(TRUE): The pins used in PORTB will be connected to VCC with a resistence between them. So, the default value is 1. 
    */
 
-   set_tris_d(0xf); //00001111: D0, D1, D2, D3 as input ports
+   set_tris_a(0xf); //00001111: D0, D1, D2, D3 as input ports
 
    output_b(0);
 
+   /*
+   LCD interaction
+   */
+   lcd_init();
+   
+   lcd_gotoxy(1,1);
+   lcd_putc("\fReady...\n");
+
    while(1){
-      //ds1307_read(&second, &minute, &hour, &day, &date, &month, &year);
-      //ds1307_read();
-      if (input(PIN_D7)){
-         while(input(PIN_D7)); //wait for PIN back to default.
+      
+      if (input(PIN_E0)){
+         while(input(PIN_E0)); //wait for PIN back to default.
 
          set_datetime();
       }
       delay_ms(100);
 
-      
+      //ds1307_read(&seconds, &minutes, &hours, &days, &date, &months, &year);
+
+      sprintf(time, "%d:%d", minutes, seconds);
+      strcpy(display,"\f");
+      strcat(display, time);
+
+      lcd_gotoxy(1,1);
+      lcd_putc(display);
    }
 
 }
